6e4603bca9ec8ddc44c6dd7958b2d37da6bef912
GBstallman <nduwimanaguymich@gmail.com>
2021-02-09 12:23:32 +0200

    models updated

--- Gestion_des_loyers/loyer/models.py
+++ Gestion_des_loyers/loyer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bureau(models.Model):
+	number_bureau = models.PositiveIntegerField()
+
+
+	def __str__(self):
+		return self.number_bureau
+
