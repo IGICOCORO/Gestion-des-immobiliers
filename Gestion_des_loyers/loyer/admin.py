70ddafe348cf238723e8fda0da8ca0a1c3f92204
GBstallman <nduwimanaguymich@gmail.com>
2021-02-12 00:38:55 +0200

    created models and admin.py

--- Gestion_des_loyers/loyer/admin.py
+++ Gestion_des_loyers/loyer/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Paiement)
+class PaiementAdmin(admin.ModelAdmin):
+	list_display = "montant_payé",
+	list_filter = "montant_payé",
+	search_field = "montant_payé", 
+	ordering = "montant_payé", 
+
+	select_related = True
+
+@admin.register(Bureau)
+class BureauAdmin(admin.ModelAdmin):
+	list_display = "num", "prix"
+	list_filter = "num", "prix"
+	search_field = "num", "prix"
+	ordering = "num", "prix"
+
+@admin.register(Immeuble)
+class ImmeubleAdmin(admin.ModelAdmin):
+	list_display = "address", "bureau"
+	list_filter = "address", "bureau",
+	search_field = "address", "bureau"
+	ordering = "address", "bureau"
+
+	select_related = True
+
+@admin.register(Proprietaire)
+class ProprietaireAdmin(admin.ModelAdmin):
+	list_display = "tel", "user"
+	list_filter = "tel", "user"
+	search_field = "tel", "user"
+	ordering = "tel",
+
+@admin.register(Locataire)
+class LocataireAdmin(admin.ModelAdmin):
+	list_display = "tel", "user"
+	list_filter = "tel","user" 
+	search_field = "tel", "user"
+	ordering = "tel", 
+
+	select_related = True
--- Gestion_des_loyers/loyer/models.py
+++ Gestion_des_loyers/loyer/models.py
@@ -1,10 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Paiement(models.Model):
+	montant_payé = models.CharField(max_length=40)
+
+	
+	def __str__(self):
+		return  {self.montant_payé}
+
 class Bureau(models.Model):
-	number_bureau = models.PositiveIntegerField()
+	num = models.CharField(max_length=40)
+	prix = models.PositiveIntegerField()
+	disponible = models.BooleanField()
+
+	def __str__(self):
+		return f'{self.num} {self.prix}'
+class Immeuble(models.Model):
+	address = models.CharField(max_length=40)
+	bureau = models.OneToOneField(Bureau,on_delete=models.CASCADE)
+	nbre_bureau = models.PositiveIntegerField()
+	def __str__(self):
+		return self.address
+
+
+class Proprietaire(models.Model):
+	user = models.OneToOneField(User,on_delete= models.CASCADE)
+	immeuble = models.OneToOneField(Immeuble,on_delete= models.PROTECT)
+	tel = models.PositiveIntegerField()
 
 
 	def __str__(self):
-		return self.number_bureau
+		return f"{self.user.first_name} {self.user.last_name}"
+
+class Locataire(models.Model):
+	user = models.OneToOneField(User,on_delete= models.CASCADE)
+	bureau = models.OneToOneField(Bureau,on_delete= models.CASCADE)
+	tel = models.PositiveIntegerField()
+
+
+	def __str__(self):
+		return f"{self.user.first_name} {self.user.last_name}"
+
 
