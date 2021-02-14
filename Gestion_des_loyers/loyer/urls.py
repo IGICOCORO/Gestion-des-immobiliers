5af2f5efa13b9f7b2176df85a0cbfd7e39c12597
GBstallman <nduwimanaguymich@gmail.com>
2021-02-15 00:29:31 +0200

    login coded

--- Gestion_des_loyers/loyer/urls.py
+++ Gestion_des_loyers/loyer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from loyer.views import *
+from django.views.generic.base import TemplateView 
 
 
 urlpatterns = [
-    path('', home, name='home')
-]
+    path('', TemplateView.as_view(template_name='loyer/login.html'), name='login'),
+]	
