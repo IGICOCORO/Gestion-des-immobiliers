d3c5c611fddc2a41aefafaf218cede854f14f25d
GBstallman <nduwimanaguymich@gmail.com>
2021-02-22 13:02:51 +0200

    added fonctionality of logging

--- Gestion_des_loyers/loyer/urls.py
+++ Gestion_des_loyers/loyer/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from loyer.views import *
-from django.views.generic.base import TemplateView 
-
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='loyer/login.html'), name='login'),
+    path('',views.login, name="login"),
+    path('home/',views.home, name='home'),
 ]	
--- Gestion_des_loyers/loyer/views.py
+++ Gestion_des_loyers/loyer/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.contrib.auth import login as auth_login ,authenticate,logout
+
+from .models import *
+from .forms import *
+
+def login(request):
+
+	if request.method == 'POST' :
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=username,password=password)
+
+		if user is not None:
+			auth_login(request, user)
+			return redirect('home')
+
+		else:
+			messages.info(request,'Username or Password is incorrect')
+	context = {}
+	return render(request,'loyer/login.html',context)
 
 def home(request):
-    
-    return render(request, 'loyer/index.html')
+	context = {}
+	return render(request,'loyer/index.html',context)
