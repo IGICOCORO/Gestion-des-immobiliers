95b3a2135414c7e13f9075adf4a1485f42eeec1d
Desos <desnduwim26@gmail.com>
2021-06-10 15:04:55 +0200

    user form created to register tanant

---
+++ Gestion_des_loyers/loyer/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+# Create your forms here.
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
+
+class OfficeForm(forms.ModelForm):
+	class Meta:
+		model = Office
+		fields = ('immeuble','num', 'picture','picture1','picture2', 'rent_amount','is_available')
+		
+
+
+class LocataireForm(forms.ModelForm):
+	class Meta:
+		model = Locataire
+		fields = ('first_name','last_name', 'phone', 'address')
+		
+
+
+class CalenderForm(forms.ModelForm):
+	class Meta:
+		model = Calender
+		fields = ('locataire','office', 'pay_inadvance','amount_inadvance', 'Paid_for_mounths')
+		
+
+
+
